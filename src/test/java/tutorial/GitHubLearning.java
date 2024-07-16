dd8d48233427fdefd4cc1979e61bdd6b442b6307
prasanthqa391 <prasanthanbu22@gmail.com>
2024-07-16 12:52:51 +0530

    this is new commit

---
+++ src/test/java/tutorial/GitHubLearning.java
@@ -0,0 +1,18 @@
+package tutorial;
+
+public class GitHubLearning {
+
+
+
+	private void monday() {
+		System.out.println("hello");
+	}
+
+
+
+	private void tuesday() {
+		System.out.println("world");
+
+	}
+
+}
